2dde6bc602a321714aa6c02defcd5a6b97b22f85
OrestisFoufris <orestis.foufris@hotmail.com>
2015-01-14 20:31:04 +0000

    test added

--- src/test/java/com/topcoder/algorithms/sorting/InsertionSortTest.java
+++ src/test/java/com/topcoder/algorithms/sorting/InsertionSortTest.java
@@ -35,4 +35,12 @@ public class InsertionSortTest {
         InsertionSort.sortArray(given);
         assertEquals(Arrays.toString(expected), (Arrays.toString(given)));
     }
+
+    @Test
+    public void sort4(){
+        int[] given = {10,9,8,7,6,5,4,3,2,1,0};
+        int[] expected = {0,1,2,3,4,5,6,7,8,9,10};
+        InsertionSort.sortArray(given);
+        assertEquals(Arrays.toString(expected), (Arrays.toString(given)));
+    }
 }
