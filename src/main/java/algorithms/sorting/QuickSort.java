b30ce51a9cadfc0e2f58f38eb23753b42d0dfcc7
OrestisFoufris <orestis.foufris@hotmail.com>
2015-02-08 22:06:28 +0000

    added randomize method

--- src/main/java/algorithms/sorting/QuickSort.java
+++ src/main/java/algorithms/sorting/QuickSort.java
@@ -2,6 +2,7 @@ package algorithms.sorting;
 
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Orestis on 02/02/2015
@@ -24,10 +25,11 @@ public class QuickSort{
         }
     }
 
-    private static <T extends Comparable<? super T>> int partition(List<T> array, int end, int arrayLength){
+    private static <T extends Comparable<? super T>> int partition(List<T> array, int start, int arrayLength){
+        randomize(arrayLength, start, array);
         T last = array.get(arrayLength-1);
-        int i = end-1;
-        for(int j = end; j < arrayLength-1; j++){
+        int i = start-1;
+        for(int j = start; j < arrayLength-1; j++){
             if(array.get(j).compareTo(last) <= -1 || array.get(j).compareTo(last) == 0){
                 i = i+1;
                 exchange(j, i, array);
@@ -42,4 +44,12 @@ public class QuickSort{
         array.set(j, array.get(i));
         array.set(i, temp);
     }
+
+    private static <T extends Comparable<? super T>> void randomize(int arrayLength, int start, List<T> array){
+        Random random = new Random();
+        int index = random.nextInt((arrayLength - start)) + start;
+        exchange(index,arrayLength-1,array);
+    }
+
+
 }
