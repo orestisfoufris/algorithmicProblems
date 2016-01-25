4422d8a56f6a53ea6b5055e2540d935298d0e691
orestisfoufris <orestis.foufris@gmail.com>
2016-01-25 23:07:41 +0000

    Priyanka and Toys problem

---
+++ src/main/java/com/hackerrank/algorithms/greedy/PriyankaAndToys.java
@@ -0,0 +1,282 @@
+package com.hackerrank.algorithms.greedy;
+
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Objects;
+
+/**
+ * @author Orestis
+ * https://www.hackerrank.com/challenges/priyanka-and-toys
+ */
+public class PriyankaAndToys {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+
+        int N = in.readInt();
+        int[] weights = new int[N];
+
+        for (int i = 0; i < N; ++i) {
+            weights[i] = in.readInt();
+        }
+        mergeSort(weights);
+
+        int count = 1;
+        int sum = weights[0] + 4;
+        for (int i = 1; i < N; ++i) {
+            if (sum < weights[i]) {
+                count++;
+                sum = weights[i] + 4;
+            }
+        }
+
+        out.print(count);
+        out.close();
+    }
+
+    public static void mergeSort(int[] a) {
+        int[] tmp = new int[a.length];
+        mergeSort(a, tmp, 0, a.length - 1);
+    }
+
+    private static void mergeSort(int[] a, int[] tmp, int left, int right) {
+        if (left < right) {
+            int center = (left + right) / 2;
+            mergeSort(a, tmp, left, center);
+            mergeSort(a, tmp, center + 1, right);
+            merge(a, tmp, left, center + 1, right);
+        }
+    }
+
+    private static void merge(int[] a, int[] tmp, int left, int right, int rightEnd) {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (a[left] <= a[right]) {
+                tmp[k++] = a[left++];
+            } else {
+                tmp[k++] = a[right++];
+            }
+        }
+
+        while (left <= leftEnd) // Copy rest of first half
+        {
+            tmp[k++] = a[left++];
+        }
+
+        while (right <= rightEnd) // Copy rest of right half
+        {
+            tmp[k++] = a[right++];
+        }
+
+        // Copy tmp back
+        for (int i = 0; i < num; i++, rightEnd--) {
+            a[rightEnd] = tmp[rightEnd];
+        }
+    }
+
+    private static class Pair implements Comparable<Pair> {
+
+        private final int index;
+        private final int value;
+
+        private Pair(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Pair other) {
+            if (this.equals(other)) {
+                return 0;
+            }
+
+            if (this.value == other.value) {
+                return Integer.compare(this.index, other.index);
+            } else {
+                return Integer.compare(this.value, other.value);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return Objects.equals(index, pair.index) &&
+                    Objects.equals(value, pair.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(index, value);
+        }
+
+    }
+    //FAST IO
+    private static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public double readDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, readInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            boolean isSpaceChar(int ch);
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+
+    }
+}
