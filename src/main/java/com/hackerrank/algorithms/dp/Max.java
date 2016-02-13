72610365bed735f2d47c846fe80a66e92c25fc4f
orestisfoufris <orestis.foufris@gmail.com>
2016-02-13 23:59:27 +0000

    Max problem

---
+++ src/main/java/com/hackerrank/algorithms/dp/Max.java
@@ -0,0 +1,36 @@
+package com.hackerrank.algorithms.dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ *  on 13/02/2016.
+ */
+public class Max {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.valueOf(in.readLine());
+        for (int k = 0; k < T; ++k) {
+            int N = Integer.valueOf(in.readLine());
+            String[] stockPrices = in.readLine().split(" ");
+
+
+            int maxPrice = 0;
+            long profit = 0;
+            for (int j = N - 1; j >= 0; --j) {
+                maxPrice = Math.max(maxPrice, Integer.valueOf(stockPrices[j]));
+                profit += Math.abs(maxPrice - Integer.valueOf(stockPrices[j]));
+            }
+
+            out.write(String.valueOf(profit) + "\n");
+        }
+
+
+        out.close();
+    }
+}
