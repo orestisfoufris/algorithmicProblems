cfc14fcd56c20af43806519098efaee8313aad10
Orestis Foufris <orestis.foufris@gmail.com>
2016-07-02 17:36:33 +0100

    getSmallest method added

--- src/main/java/ctci/chapter5/Question3.java
+++ src/main/java/ctci/chapter5/Question3.java
@@ -13,32 +13,66 @@ public final class Question3 {
         int onesCount = 0;
 
         while((temp & 1) == 0 && temp != 0) {
-            // System.out.println(Integer.toBinaryString(temp));
             zerosCount++;
             temp >>= 1;
         }
-
-        // System.out.println(Integer.toBinaryString(temp));
 
         while ((temp & 1) == 1) {
             onesCount++;
             temp >>= 1;
         }
 
-        int add = zerosCount + onesCount;
+        int index = zerosCount + onesCount;
 
-        if (add == 31 || add == 0) {
+        if (index == 31 || index == 0) {
             return -1; // can't do it
         }
 
-        number = add | (1 << add);
-        number &= ~((1 << add) - 1);
+        number = index | (1 << index);
+        number &= ~((1 << index) - 1);
         number |= (1 << (onesCount - 1)) - 1;
 
         return number;
     }
 
+    public static int getSmallest(int number) {
+        int temp = number;
+        int zeros = 0;
+        int ones = 0;
+
+        // count ones
+        while ((temp & 1) == 1) {
+            ones++;
+            temp = temp >> 1;
+        }
+
+        if (temp == 0) { // there was no trailing 1s
+            return -1;
+        }
+
+        // count zeros
+        while ((temp & 1) == 0 && temp != 0) {
+            zeros++;
+            temp = temp >> 1;
+        }
+
+        int index = zeros + ones;
+        int mask = ((~0) << index + 1);
+        number = number & mask; // clear ones from index until the end
+        number = number | (1 << ones + 1) - 1;
+
+        return number << (zeros - 1);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(getLargest(5));
+        System.out.println(getLargest(7));
+        System.out.println(getSmallest(11) + "\n");
+
+        System.out.println(getLargest(8));
+        System.out.println(getSmallest(16) + "\n");
+
+        System.out.println(getLargest(15));
+        System.out.println(getSmallest(23) + "\n");
     }
 }
