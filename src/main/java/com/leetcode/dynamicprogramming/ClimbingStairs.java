ae3fc0e55aaf7716ecf9ad0ec40b35528e37f71a
Orestis Foufris <orestis.foufris@gmail.com>
2016-07-26 22:22:22 +0100

    Climbing Stairs problem

---
+++ src/main/java/com/leetcode/dynamicprogramming/ClimbingStairs.java
@@ -0,0 +1,31 @@
+package com.leetcode.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+public class ClimbingStairs {
+
+    public static int climbStairs(int n) {
+        Map<Integer, Integer> computed = new HashMap<>();
+        return climb(n, computed);
+    }
+
+    private static int climb(int n, Map<Integer, Integer> computed) {
+        if (n == 0) { return 0; }
+        if (n == 1) { return 1; }
+        if (n == 2) { return 2; }
+
+        if (computed.containsKey(n)) {
+            return computed.get(n);
+        } else {
+            int result = climb(n - 1, computed) + climb(n - 2, computed);
+            computed.put(n, result);
+
+            return result;
+        }
+
+    }
+}
