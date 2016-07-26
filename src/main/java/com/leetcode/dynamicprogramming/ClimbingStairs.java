1f55f43c79c40cd55850d4de706e7ea7709c8e1e
Orestis Foufris <orestis.foufris@gmail.com>
2016-07-27 00:05:31 +0100

    fixing base case

--- src/main/java/com/leetcode/dynamicprogramming/ClimbingStairs.java
+++ src/main/java/com/leetcode/dynamicprogramming/ClimbingStairs.java
@@ -14,8 +14,7 @@ public class ClimbingStairs {
     }
 
     private static int climb(int n, Map<Integer, Integer> computed) {
-        if (n == 0) { return 0; }
-        if (n == 1) { return 1; }
+        if (n == 1 || n == 0) { return 1; }
         if (n == 2) { return 2; }
 
         if (computed.containsKey(n)) {
