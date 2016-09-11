633e3dd0c8ce3f686606148040f10d8095555282
Orestis Foufris <orestis.foufris@gmail.com>
2016-09-11 16:12:59 +0100

    "isIsomorphic" problem

---
+++ src/main/java/com/leetcode/IsomorphicStrings.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/isomorphic-strings/
+ */
+public class IsomorphicStrings {
+
+    public boolean isIsomorphic(String s, String t) {
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (!Objects.equals(map1.put(s.charAt(i), i), map2.put(t.charAt(i), i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
