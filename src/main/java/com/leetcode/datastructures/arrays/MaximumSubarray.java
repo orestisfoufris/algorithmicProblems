1ac891ba66a5e6b71d772122fb509a28234fddf5
Orestis Foufris <orestis.foufris@gmail.com>
2016-09-13 22:43:14 +0100

    Max subarray improvement

--- src/main/java/com/leetcode/datastructures/arrays/MaximumSubarray.java
+++ src/main/java/com/leetcode/datastructures/arrays/MaximumSubarray.java
@@ -6,19 +6,12 @@ package com.leetcode.datastructures.arrays;
 public class MaximumSubarray {
 
     public int maxSubArray(int[] nums) {
-        int min = Integer.MIN_VALUE;
-        int sum = 0;
-        int max = 0;
+        int sum = nums[0];
+        int max = nums[0];
 
-        for (int i = 0; i < nums.length; ++i) {
-            max = Math.max(0, max + nums[i]);
+        for (int i = 1; i < nums.length; ++i) {
+            max = Math.max(nums[i], max + nums[i]);
             sum = Math.max(max, sum);
-
-            min = Math.max(min, nums[i]);
-        }
-
-        if (sum == 0 && nums.length > 0) {
-            return min;
         }
 
         return sum;
