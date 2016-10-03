bed703541e96306a07a84d824d65fb9772e3ac9f
Orestis Foufris <orestis.foufris@gmail.com>
2016-10-03 21:11:36 +0100

    removing unnecessary loop

--- src/main/java/com/leetcode/dynamicprogramming/RangeSumQuery2DImmutable.java
+++ src/main/java/com/leetcode/dynamicprogramming/RangeSumQuery2DImmutable.java
@@ -17,10 +17,6 @@ public class RangeSumQuery2DImmutable {
 
         dp = new int[matrix.length + 1][matrix[0].length + 1];
 
-        for (int i = 0; i < matrix.length; ++i) {
-            dp[i][0] = matrix[i][0];
-        }
-
         for (int i = 1; i <= matrix.length; ++i) {
             for (int j = 1; j <= matrix[0].length; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1] ;
