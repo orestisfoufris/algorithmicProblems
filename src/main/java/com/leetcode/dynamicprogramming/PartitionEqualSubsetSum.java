995b975e4574fd4d5866bde7c61ff0ad5d5a5c9c
Orestis Foufris <orestis.foufris@gmail.com>
2016-10-29 20:03:08 +0100

    "bottoms up solution added"

--- src/main/java/com/leetcode/dynamicprogramming/PartitionEqualSubsetSum.java
+++ src/main/java/com/leetcode/dynamicprogramming/PartitionEqualSubsetSum.java
@@ -44,4 +44,30 @@ public class PartitionEqualSubsetSum {
         return result;
     }
 
+    public boolean canPartitionBottomsUp(int[] nums) {
+        // check edge case
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+
+        int target = 0;
+        for (int num : nums) {
+            target += num;
+        }
+        if (target % 2 != 0) {
+            return false;
+        }
+        target /= 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = target; j >= nums[i-1]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i-1]];
+            }
+        }
+
+        return dp[target];
+    }
+
 }
