58a5d3f680d520b0f2da2654c1c1b74fd9de1db2
Orestis Foufris <orestis.foufris@gmail.com>
2016-11-12 22:50:33 +0000

    "num of Islands bfs"

--- src/main/java/com/leetcode/graphs/NumberOfIslands.java
+++ src/main/java/com/leetcode/graphs/NumberOfIslands.java
@@ -1,5 +1,9 @@
 package com.leetcode.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/number-of-islands/
  * 16/10/2016.
@@ -39,6 +43,77 @@ public class NumberOfIslands {
             if (y - 1 >= 0) {
                 dfs(matrix, x, y - 1);
             }
+        }
+    }
+
+
+    public static int numIslandsBfs(char[][] grid) {
+        int counter = 0;
+
+        for (int x = 0; x < grid.length; ++x) {
+            for (int y = 0; y < grid[x].length; ++y) {
+                if (grid[x][y] == '1') {
+                    counter++;
+                    bfs(grid, x, y);
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    private static void bfs(char[][] grid, int x, int y) {
+
+        Queue<Point> q = new ArrayDeque<>();
+        q.offer(new Point(x, y));
+        grid[x][y] = '0';
+
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            if(p.x > 0 && grid[p.x - 1][p.y] == '1') {
+                q.offer(new Point(p.x - 1, p.y));
+                grid[p.x - 1][p.y] = '0';
+            }
+
+            if (p.x < grid.length - 1 && grid[p.x + 1][p.y] == '1') {
+                q.offer(new Point(p.x + 1, p.y));
+                grid[p.x + 1][p.y] = '0';
+            }
+
+            if (p.y > 0 && grid[p.x][p.y - 1] == '1') {
+                q.offer(new Point(p.x, p.y - 1));
+                grid[p.x][p.y - 1] = '0';
+            }
+
+            if (p.y < grid[0].length - 1 && grid[p.x][p.y + 1] == '1') {
+                q.offer(new Point(p.x, p.y + 1));
+                grid[p.x][p.y + 1] = '0';
+            }
+        }
+    }
+
+    private static class Point {
+        final int x;
+        final int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x &&
+                    y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 
