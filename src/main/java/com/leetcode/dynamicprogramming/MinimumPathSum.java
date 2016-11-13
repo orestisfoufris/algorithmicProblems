ac58bf1c0ea833d05deb4871d5f638253e893592
Orestis Foufris <orestis.foufris@gmail.com>
2016-11-13 21:58:17 +0000

    top down solution to "Min Path Sum" problem

--- src/main/java/com/leetcode/dynamicprogramming/MinimumPathSum.java
+++ src/main/java/com/leetcode/dynamicprogramming/MinimumPathSum.java
@@ -1,5 +1,7 @@
 package com.leetcode.dynamicprogramming;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/minimum-path-sum/
  */
@@ -32,6 +34,34 @@ public class MinimumPathSum {
         }
 
         return grid[m - 1][n - 1];
+    }
+
+    public int minPathSumTopDown(int[][] grid) {
+        int[][] computed = new int[grid.length + 1][grid[0].length + 1];
+        for (int[] ar : computed) {
+            Arrays.fill(ar, -1);
+        }
+
+        return solve(grid, 0, 0, computed);
+    }
+
+    private int solve(int[][] g, int i, int j, int[][] computed) {
+        if (i == g.length - 1 && j == g[i].length - 1) {
+            return g[i][j];
+        }
+
+        if (i >= g.length || j >= g[i].length) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (computed[i][j] != -1) {
+            return computed[i][j];
+        }
+
+        int result = g[i][j] + Math.min(solve(g, i + 1, j, computed),
+                                        solve(g, i, j + 1, computed));
+
+        return computed[i][j] = result;
     }
 
 }
