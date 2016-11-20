73c3f7d97738e6da93cecef0f2a55fe0070a5a2d
Orestis Foufris <orestis.foufris@gmail.com>
2016-11-20 23:50:35 +0000

    "DFS: Connected Cell in a Grid" problem

---
+++ src/main/java/com/hackerrank/algorithms/graphtheory/ConnectedCellInAGrid.java
@@ -0,0 +1,228 @@
+package com.hackerrank.algorithms.graphtheory;
+
+import javax.annotation.Generated;
+import java.io.*;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-connected-cell-in-a-grid
+ */
+public class ConnectedCellInAGrid {
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+
+        int rows = in.readInt();
+        int columns = in.readInt();
+
+        int[][] matrix = new int[rows][columns];
+        boolean[][] visited = new boolean[rows][columns];
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                matrix[i][j] = in.readInt();
+            }
+        }
+
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                if (matrix[i][j] == 1) {
+                    max = Math.max(max, dfs(matrix, i, j, visited));
+                } else {
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        out.print(max);
+        out.close();
+    }
+
+    private static long dfs(int[][] matrix, int x, int y, boolean[][] visited) {
+        if (x < matrix.length && y < matrix[0].length && x >= 0 && y >= 0 &&
+                matrix[x][y] != 0 && !visited[x][y]) {
+
+            visited[x][y] = true; // mark tile visited
+
+            return 1 + dfs(matrix, x + 1, y, visited) +
+                       dfs(matrix, x, y + 1, visited) +
+                       dfs(matrix, x - 1, y, visited) +
+                       dfs(matrix, x, y - 1, visited) +
+                       dfs(matrix, x - 1, y - 1, visited) +
+                       dfs(matrix, x + 1, y - 1, visited) +
+                       dfs(matrix, x - 1, y + 1, visited) +
+                       dfs(matrix, x + 1, y + 1, visited);
+        }
+
+        return 0;
+    }
+
+    //FAST IO
+    @Generated("Fast IO")
+    private static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1 << 10];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public double readDouble() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, readInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, readInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public long readLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+            boolean isSpaceChar(int ch);
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+
+    }
+}
