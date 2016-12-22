fa863b8fe261cc39c6b77953c85dd6dc1704fae2
Orestis Foufris <orestis.foufris@gmail.com>
2016-12-22 14:07:49 +0000

    "Repeated Substring Pattern" problem

---
+++ src/main/java/com/leetcode/datastructures/arrays/RepeatedSubstringPattern.java
@@ -0,0 +1,32 @@
+package com.leetcode.datastructures.arrays;
+
+/**
+ * https://leetcode.com/problems/repeated-substring-pattern/
+ * 22/12/2016.
+ */
+public class RepeatedSubstringPattern {
+
+    public boolean repeatedSubstringPattern(String str) {
+        int len = str.length();
+
+        for (int i = len / 2; i >= 1; --i) {
+            if (len % i == 0) {
+                int middle = len / i;
+                String substring = str.substring(0, i);
+
+                int j;
+                for(j = 1; j < middle; ++j) {
+                    if(!substring.equals(str.substring(j * i,i + j * i))) {
+                        break;
+                    }
+                }
+
+                if(j == middle)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+}
