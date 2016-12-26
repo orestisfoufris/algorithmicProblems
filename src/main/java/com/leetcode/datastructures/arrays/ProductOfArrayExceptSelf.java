efdbc7bee396d7209230e8574c2d771055997a04
Orestis Foufris <orestis.foufris@gmail.com>
2016-12-26 20:41:43 +0000

    "Product O fArray Except Self"  problem refactoring

--- src/main/java/com/leetcode/datastructures/arrays/ProductOfArrayExceptSelf.java
+++ src/main/java/com/leetcode/datastructures/arrays/ProductOfArrayExceptSelf.java
@@ -8,25 +8,24 @@ import java.util.Arrays;
  */
 public class ProductOfArrayExceptSelf {
 
-    public static int[] productExceptSelf(int[] nums) {
-        int N = nums.length;
-        int[] result = new int[N];
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        output[0] = 1;
 
-        result[0] = 1;
-        for (int i = 1; i < N; i++) {
-            // calculate product of all elements left to i
-            result[i] = result[i - 1] * nums[i - 1];
+        for (int i = 1; i < n; ++i) {
+            output[i] = output[i - 1] * nums[i - 1];
         }
 
-        int right = 1;
-        for (int i = N - 1; i >= 0; i--) {
-            // calculate product of all elements right to i
-            // using the product from left to i
-            result[i] *= right;
+        int right = nums[n - 1];
+        System.out.println(Arrays.toString(output));
+
+        for (int i = n - 2; i >= 0; --i) {
+            output[i] = output[i] * right;
             right *= nums[i];
         }
 
-        return result;
+        return output;
     }
 
 }
