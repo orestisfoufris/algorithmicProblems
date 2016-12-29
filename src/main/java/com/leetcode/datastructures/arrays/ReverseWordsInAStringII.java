af9484d51c477c644de57246b0ea90f1b2f68c24
Orestis Foufris <orestis.foufris@gmail.com>
2016-12-29 22:38:31 +0000

    "Reverse Words In A String II" problem

---
+++ src/main/java/com/leetcode/datastructures/arrays/ReverseWordsInAStringII.java
@@ -0,0 +1,26 @@
+package com.leetcode.datastructures.arrays;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/reverse-words-in-a-string-ii/
+ * 29/12/2016.
+ */
+public class ReverseWordsInAStringII {
+
+    public static void reverseWords(char[] s) {
+        String[] ar = new String(s).trim().split(" ");
+        int l = 0;
+
+        for (int j = ar.length - 1; j >= 0; --j) {
+            String current = ar[j];
+            for (int i = 0; i < current.length(); ++i) {
+                s[l++] = current.charAt(i);
+            }
+
+            if (j > 0) {
+                s[l++] = ' ';
+            }
+        }
+    }
+}
