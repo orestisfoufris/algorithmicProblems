9d7333d7738a0fdbc2f150af8bc1b6d9814aaa72
Orestis Foufris <orestis.foufris@gmail.com>
2017-05-10 21:30:18 +0100

    added "Convert BST to Greater Tree" problem

---
+++ src/main/java/com/leetcode/datastructures/trees/ConvertBSTToGreaterTree.java
@@ -0,0 +1,33 @@
+public class ConvertBSTToGreaterTree {
+    private int count = 0;
+    private int prev = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+      if (root == null) {
+        return null;
+      }
+
+      traverse(root);
+      traverseAndSubtract(root);
+
+      return root;
+    }
+
+    private void traverseAndSubtract(TreeNode node){
+      if (node != null) {
+        traverseAndSubtract(node.left);
+        int temp = node.val;
+        node.val = count - prev;
+        prev += temp;
+        traverseAndSubtract(node.right);
+      }
+    }
+
+    private void traverse(TreeNode node){
+      if (node != null) {
+        count += node.val;
+        traverse(node.right);
+        traverse(node.left);
+      }
+    }
+}
