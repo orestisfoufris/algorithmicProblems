a5f19628ffe353376d65a1fb8797d2c6156fafac
orestisfoufris <orestis.foufris@hotmail.com>
2018-01-21 22:51:59 +0000

    adding problem

--- src/main/java/com/leetcode/datastructures/arrays/ContainerWithMostWater.java
+++ src/main/java/com/leetcode/datastructures/arrays/ContainerWithMostWater.java
@@ -1,4 +1,28 @@
 package com.leetcode.datastructures.arrays;
 
+/**
+ * https://leetcode.com/problems/container-with-most-water/description/
+ */
 public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int max = Integer.MIN_VALUE;
+
+        while (start < end) {
+            int minHeight = Math.min(height[start], height[end]);
+            int area = minHeight * (end - start);
+            max = Math.max(max, area);
+
+            if (height[start] > height[end]) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+
+        return max;
+    }
+
 }
