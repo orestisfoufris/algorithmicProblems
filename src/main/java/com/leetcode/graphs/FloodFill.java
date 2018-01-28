6645c6d29569f3bdedba6bc4eea33ea1b269dcb4
orestisfoufris <orestis.foufris@hotmail.com>
2018-01-28 23:53:33 +0000

    adding "FloodFill" problem

---
+++ src/main/java/com/leetcode/graphs/FloodFill.java
@@ -0,0 +1,30 @@
+package com.leetcode.graphs;
+
+/**
+ * https://leetcode.com/problems/flood-fill/description/
+ */
+public class FloodFill {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int[][] seen = new int[image.length][image.length];
+        dfs(image, sr, sc, newColor, seen, image[sr][sc]);
+
+        return image;
+    }
+
+    private void dfs(int[][] image, int row, int column, int color, int[][] seen, int start) {
+        if (row >= image.length || column >= image.length) { return; }
+        if (row < 0 || column < 0) { return; }
+        if (seen[row][column] == 1) { return; }
+        if (image[row][column] != start) { return; }
+
+        image[row][column] = color;
+        seen[row][column] = 1;
+
+        dfs(image, row + 1, column, color, seen, start);
+        dfs(image, row - 1, column, color, seen, start);
+        dfs(image, row, column + 1, color, seen, start);
+        dfs(image, row, column - 1, color, seen, start);
+
+    }
+}
